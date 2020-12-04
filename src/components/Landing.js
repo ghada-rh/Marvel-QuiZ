f3a690804ef5eed650f8d938cd1552bd6fdd451a
ghada-rh <ghadarhouuma@gmail.com>
2020-12-04 22:05:45 +0100

    1 file modified

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,30 @@
-import React, {useRef, useEffect } from "react";
+import React, {useRef, useEffect, useState, Fragment } from "react";
 
 const Landing=()=> {
   const refWolverine = useRef(null);
+  const [btn, setBtn] = useState(false);
+  
 
   useEffect(()=>{
     refWolverine.current.classList.add("startingImg");
     setTimeout(()=>{
        refWolverine.current.classList.remove("startingImg");
-    }, 3000)
+       setBtn(true);
+    }, 1000)
   })
+
+  const displayBtn = btn && (
+                   <Fragment>
+                           <div className="leftBox">
+                            <button className="btn-welcome"> Inscription </button> 
+                          </div>
+                          <div className="rightBox">
+                              <button className="btn-welcome"> Connexion </button>
+                        </div>
+                  </Fragment> )
   return (
     <main ref ={refWolverine} className="welcomePage"> 
-       <div className="leftBox">
-          <button className="btn-welcome"> Inscription </button> 
-       </div>
-       <div className="leftBox">
-          <button className="btn-welcome"> Connexion </button>
-       </div>
-    landing page
+       {displayBtn}
     </main>
   );
 }
