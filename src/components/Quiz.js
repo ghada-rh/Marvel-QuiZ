73a15ed13e9b3d9cd8c5c6b4f5d427c3e214197c
ghada-rh <ghadarhouuma@gmail.com>
2020-12-11 21:41:05 +0100

    1 file modified, 1 added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,23 @@
 import React, {Component} from "react";
 import Levels from './Levels';
 import ProgressBar from './ProgressBar';
+import {QuizMarvel} from './QuizMarvel';
+
 class Quiz extends Component{
+  state = {
+    levelsNames: ['debutant', 'confirmé', 'expert'],
+    quizLevel : 0,
+    maxQuestions: 10,
+    storedQuestions: []
+  }
+
+  loadQuestions = quiz =>{
+     console.log(level);
+  }
+
+  componentDidMount(){
+      this.loadQuestions(this.state.levelsNames[this.state.quizLevel]);
+  }
   render(){
   const {pseudo}= this.props.userData;
   console.log(this.props);
