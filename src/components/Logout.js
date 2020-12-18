f18e802f2fda12760cedd51a2f30738285256875
ghada-rh <ghadarhouuma@gmail.com>
2020-12-18 13:53:52 +0100

    1 file modified

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from "react";
 import {FirebaseContext} from './Firebase';
-
+import ReactTooltip from 'react-tooltip';
 const Logout = () => {
   const [checked, setChecked] = useState(false);
   const firebase = useContext(FirebaseContext);
@@ -17,8 +17,12 @@ const Logout = () => {
   return (
       <div className="logoutContainer">
         <label className="switch"> 
-        <input type="checkbox" checked={checked} onChange={hundleChange}/>
-        <span className="slider round"></span>
+            <input type="checkbox" checked={checked} onChange={hundleChange}/>
+            <span className="slider round" data-tip="Déconnexion"></span>
+            <ReactTooltip 
+              place="left" 
+              effect="solid"
+            />
         </label>
       </div>
   );
