6f15117f81aa79777e8ea0c5a9fafdcfc3741171
ghada-rh <ghadarhouuma@gmail.com>
2020-12-19 19:16:54 +0100

    1 file modified

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyA2ps_L6m0-kr6vP-CEZzFtT3aOPWHJ_R8",
-    authDomain: "marvel-quiz-c6480.firebaseapp.com",
-    projectId: "marvel-quiz-c6480",
-    storageBucket: "marvel-quiz-c6480.appspot.com",
-    messagingSenderId: "904217297559",
-    appId: "1:904217297559:web:83bdf182265908e5f71fd2"
+    apiKey: "AIzaSyAqy_4F-_Fk18uKm1X6tzAY2IuLSRx_bsQ",
+    authDomain: "quiz-marvel-c52db.firebaseapp.com",
+    projectId: "quiz-marvel-c52db",
+    storageBucket: "quiz-marvel-c52db.appspot.com",
+    messagingSenderId: "874024742103",
+    appId: "1:874024742103:web:5476105b66b1aeede15492"
   };
 
 class Firebase {
